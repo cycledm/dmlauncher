a06c663a11910a6fb57b3224c3edfdcc55a45835
CycleDM <cycledm0508@gmail.com>
2025-06-14 16:57:15 +0900

    feat: 添加 Downloads 页面并更新路由配置

---
+++ src/renderer/src/pages/Downloads/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import clsx from "clsx";
+
+export default function Downloads(): React.JSX.Element {
+  return <p className={clsx()}>Downloads</p>;
+}
--- src/renderer/src/routes.ts
+++ src/renderer/src/routes.ts
@@ -1,15 +1,16 @@
+import React from "react";
 import { RouteObject } from "react-router";
 import Template from "@renderer/pages/Template";
 import Home from "@renderer/pages/Home";
 import Java from "@renderer/pages/Java";
-import React from "react";
+import Downloads from "@renderer/pages/Downloads";
 
 const routes: RouteObject[] = [
   { id: "home", path: "/", Component: Home },
   { id: "java", path: "/java", Component: Java },
-  { id: "downloads", path: "/downloads", Component: React.Fragment },
-  { id: "settings", path: "/settings", Component: React.Fragment },
-  { id: "template", path: "/template", Component: Template }
+  { id: "downloads", path: "/downloads", Component: Downloads },
+  { id: "template", path: "/template", Component: Template },
+  { id: "settings", path: "/settings", Component: React.Fragment }
 ];
 
 export default routes;
